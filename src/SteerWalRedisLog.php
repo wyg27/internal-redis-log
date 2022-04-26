2bff30499738e1076618f8409076894afc62f093
Max(Yiguo Wang) <max.wang@facedrive.com>
2022-04-25 21:02:34 -0400

    first try

---
+++ src/SteerWalRedisLog.php
@@ -0,0 +1,19 @@
+<?php
+
+class SteerWalRedisLog
+{
+    protected string $channel_id;
+
+    public function __construct(string $channel_id)
+    {
+        $this->channel_id = trim($channel_id);
+    }
+
+    /**
+     * @return string
+     */
+    public function getChannelId(): string
+    {
+        return $this->channel_id;
+    }
+}
